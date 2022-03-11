81fe759b668f746605642023baab42f3291a1f29
Ogu <august.gros@gmail.com>
2022-03-11 11:54:14 +0100

    add preview de la boite d'ajout

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
+/* Switch display none/flex */
 const btnPlus = document.querySelector('#btnAdd')
 const main = document.querySelector('#boxMain')
 const test = document.querySelector('#test')
-
 
 btnPlus.addEventListener('click', function(){
     main.classList.add("hidden");
@@ -9,4 +9,19 @@ btnPlus.addEventListener('click', function(){
 
     test.classList.add("flex");
     test.classList.remove("hidden");
+})
+
+/* Preview de l'input file */
+const inputFile = document.querySelector("#fileAdd");
+const preview = document.querySelector("#preview");
+
+inputFile.addEventListener('change', function(){
+    while(preview.firstChild) {
+      preview.removeChild(preview.firstChild);
+    }
+    let curFiles = inputFile.files;
+    let image = document.createElement('img');
+    image.src = window.URL.createObjectURL(curFiles[0]);
+    image.classList.add('rounded-full','w-24','h-24','border-2','border-white','bg-white');
+    preview.appendChild(image);
 })
