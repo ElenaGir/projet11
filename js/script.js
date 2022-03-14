0d92f1230bff40be30edcc27cf8ce8aa61367c49
ElenaGir <elena.girard@gmail.com>
2022-03-14 09:05:38 +0100

    responsive

--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@ document.getElementById('nav-toggle').onclick = function(){
     
     document.getElementById("nav-content").classList.toggle("hidden");
 }
+const nav = document.querySelector('#navbar');
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 150){
+        nav.classList.add('scroll');
+
+    }
+    else{
+        nav.classList.remove('scroll');
+    }
+    
+});
