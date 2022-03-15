f35a7d09c3276b4ed2b6cb358d1836f889f2492c
Ogu <august.gros@gmail.com>
2022-03-15 13:44:11 +0100

    boite add fonctionnelle sans img

--- php/addBO.php
+++ php/addBO.php
@@ -1,5 +1,16 @@
 <?php
 
+$host = 'localhost';
+$dbname = 'projet11';
+$username = 'phpmyadmin';
+$password = 'apache2luxe';
+
+try{
+	$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+
+
+
 $regleURL = "/^[a-zA-Z0-9:.\/-]+$/";
 $regleNomPrenom = "/^[a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ'-]+$/";
 $reglePrix = "/^[0-9]+$/";
@@ -13,8 +24,28 @@ if((isset($_POST['nom']))&&(preg_match($regleNomPrenom, $_POST['nom']))){
 if((isset($_POST['prix']))&&(preg_match($reglePrix, $_POST['prix']))){
     $prix = $_POST['prix'];
 }
-if(($imgBO)&&($nom)&&($prix)){
+if(isset($_POST['source'])){
+    $source = $_POST['source'];
+}
+if(($imgBO)&&($nom)&&($prix)&&($source)){
     $res = array('validation' => "Cbon");
     echo json_encode($res);
+
+/*     $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]); */
+
+    $updateprod = $conn->prepare('INSERT INTO Produits (Noms, Images, Prix, Sources) VALUES (?, ?, ?, ?)');
+    $updateprod->execute(array($nom, "slt", $prix, $source));
 }
+
+
+
+
+if($conn === false){
+	die("Erreur");
+}
+}catch (PDOException $e){
+  	die("Impossible de se connecter à la base de données $dbname :" . $e->getMessage());
+}
+
 ?>
