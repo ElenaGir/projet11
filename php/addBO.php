0ffa334821d35f6bcc6ff0ac4a328b0d25709501
Ogu <august.gros@gmail.com>
2022-03-15 11:09:46 +0100

    box d'ajout qui marche

--- php/addBO.php
+++ php/addBO.php
@@ -1,20 +1,20 @@
 <?php
 
-/* $regleURL = "/^[a-zA-Z0-9:.\/-]+$/";
-$regleNom = "/^[a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ'-]+$/";
-$reglePrix = "/^[0-9]+$/"; */
+$regleURL = "/^[a-zA-Z0-9:.\/-]+$/";
+$regleNomPrenom = "/^[a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ'-]+$/";
+$reglePrix = "/^[0-9]+$/";
 
-if(isset($_POST['imgBO'])){
-    $imgBO = $_POST['imgBO'];   
+if((isset($_FILES['imgBO']))&&(preg_match($regleURL, $_FILES['imgBO']["tmp_name"]))){
+    $imgBO = $_FILES['imgBO'];
 }
-if(isset($_POST['nom'])){
+if((isset($_POST['nom']))&&(preg_match($regleNomPrenom, $_POST['nom']))){
     $nom = $_POST['nom'];
 }
-if(isset($_POST['prix'])){
+if((isset($_POST['prix']))&&(preg_match($reglePrix, $_POST['prix']))){
     $prix = $_POST['prix'];
 }
 if(($imgBO)&&($nom)&&($prix)){
-    $res = array('validation' => "Email envoyé");
+    $res = array('validation' => "Cbon");
     echo json_encode($res);
 }
 ?>
